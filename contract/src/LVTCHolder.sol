9802bb49450643da10bf285e105e4897ad891bbb
eric <ericzhu@lives.one>
2018-07-25 14:58:07 +0800

    add LVTCHolder contract to burn LVTC

---
+++ contract/src/LVTCHolder.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.11;
+
+contract LVTCHolder {
+    string public info;
+
+    function LVTCHolder() public {
+        info = "This contract is used to burn LVTC token";
+    }
+}
